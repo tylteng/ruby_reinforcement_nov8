4362f2a616106f8ce655ad92cb64d63dfcc125c0
tylteng <teng.ty4@gmail.com>
2017-11-09 10:21:06 -0500

    re.. completed exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,10 +1,22 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array_odd_sum = 0
+odd_sum = 0
 
 array.each do |x|
   if x == 1 || x % 2 == 1
-    array_odd_sum += x
+    odd_sum += x
   end
 end
 
-p array_odd_sum
+
+def array_odd_sum(array)
+  odd_sum = 0
+
+  array.each do |x|
+    if x == 1 || x % 2 == 1
+      odd_sum += x
+    end
+  end
+  return odd_sum
+end
+
+p array_odd_sum(array)
